93fa2595ec6a5f109898f4d16d33f6e47f436259
SuyashJadhav1021 <suyashjadhav2831@gmail.com>
2024-08-13 09:49:08 +0530

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel, qui nam
+        deserunt ad sit aliquid consectetur ab quo ea. Eveniet, eos? Praesentium
+        ducimus ut libero distinctio, ratione architecto consectetur cumque
+        eligendi vitae quos nulla pariatur facere perspiciatis, dolore natus
+        necessitatibus!
+      </p>
+    </section>
+  );
+};
+
+export default About;
